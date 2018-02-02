64f4618fbae56c38d81274516430cf400567d10a
Cristian Peralta <cbperaltasegura@gmail.com>
2018-02-02 08:50:29 -0500

    update params to connect to db

--- database/connect.js
+++ database/connect.js
@@ -1,16 +1,13 @@
 import mongodb from  "mongodb";
+
 const MongoClient = mongodb.MongoClient;
 
-let host   = "127.0.0.1";
-let port   = 27017;
-let nameDb = "ourDb";
-var myCollection;
-exports.connect =  function () {
+exports.connect =  function (host,port,nameDb) {
 
   MongoClient.connect("mongodb://"+host+":"+port+"/", function(err, client) {
     if(err)
-    throw err;
-    console.log("connected to the mongoDB !");
+      throw err;
+    console.log("Connected to the mongoDB ! -> mongodb://"+host+":"+port+"/"+nameDb);
     let db = client.db(nameDb);
     global.db = client;
   });
--- www.js
+++ www.js
@@ -1,7 +1,13 @@
 import server from "./server";
-import db from "./database/config";
+import db from "./database/connect";
 
 const port = 3000;
 const hostname = "0.0.0.0";
-db.connect();
+
+let hostDb   = "127.0.0.1";
+let portDb   = 27017;
+let nameDb = "ourDb";
+
+db.connect(hostDb,portDb,nameDb);
+
 server.init(port,hostname);
